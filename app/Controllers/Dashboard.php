588b9d7637c3dc725ca07ae30fed2d37efb85082
Serhat Capan <serhat@databauer.ch>
2024-03-08 10:43:19 +0100

    dashboard update

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2,17 +2,50 @@
 
 namespace App\Controllers;
 
+use App\Models\CostCenterModel;
+use App\Models\WorkhourModel;
+use CodeIgniter\I18n\Time;
+use Exception;
+
 class Dashboard extends BaseController
 {
+    private WorkhourModel $workhourmodel;
+    private CostCenterModel $costcentermodel;
+
     public function __construct()
     {
-
+        $this->workhourmodel = new WorkhourModel();
+        $this->costcentermodel = new CostCenterModel();
     }
 
+    /**
+     * @throws Exception
+     */
     public function index(): string
     {
+
+       $costcenters = $this->costcentermodel
+            ->select('*')
+            ->join('ipa_workhour', 'ipa_costcenter.id = ipa_workhour.id_costcenter', 'left')
+            ->get()->getResultArray();
+
+       $table = get_table_template();
+
+       foreach ($costcenters as $costcenter) {
+           $table->addRow($costcenter['name'], $costcenter['hours']);
+           $table->addRow($costcenter['name'], $costcenter['hours']);
+       }
+
+       $data = [
+           'table' => $table->generate()
+       ];
+
         return view('partials/header') .
-            view('home') .
+            view('/dashboard/index', $data) .
             view('partials/footer');
+    }
+
+    public function get_card_date() {
+
     }
 }
---
+++ app/Views/dashboard/index.php
@@ -0,0 +1,20 @@
+<p class="uk-text-small uk-margin-remove">Dashboard</p>
+<h1 class="uk-margin-remove-top">Arbeitszeiterfassung</h1>
+
+<div class="uk-margin-medium-top">
+    <button class="uk-button uk-button-primary">Neuen Tag erfassen</button>
+    <div class="uk-card uk-card-default uk-width-1-1">
+        <div class="uk-card-header">
+            <div class="uk-grid-small uk-flex-middle" uk-grid>
+                <div class="uk-width-expand">
+                    <div class="uk-card-badge uk-label">- 10h</div>
+                    <h3 class="uk-card-title uk-margin-remove-bottom">Dienstag</h3>
+                    <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016 </time></p>
+                </div>
+            </div>
+        </div>
+        <div class="uk-card-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </div>
+    </div>
+</div>
